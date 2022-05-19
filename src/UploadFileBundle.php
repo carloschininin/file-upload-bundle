2210d94577bf354ad8ce85628ee7a6f2f4e490c0
Carlos Chininin <carloschininin@gmail.com>
2022-05-18 21:48:50 -0500

    add bundle

--- src/UploadFileBundle.php
+++ src/UploadFileBundle.php
@@ -4,9 +4,26 @@ declare(strict_types=1);
 
 namespace CarlosChininin\UploadFile;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class UploadFileBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $this->addRegisterMappingsPass($container);
+    }
 
+    private function addRegisterMappingsPass(ContainerBuilder $container)
+    {
+        $mappings = [
+            realpath(__DIR__.'/Resources/config/doctrine-mapping') => 'CarlosChininin\UploadFile\Model',
+        ];
+
+        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
+        }
+    }
 }
