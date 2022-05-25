1669be1159695828708c1b266cb8212085777d3b
Carlos Chininin <carloschininin@gmail.com>
2022-05-24 22:41:08 -0500

    add twig extension webpath

---
+++ src/Twig/FileUploadTwigExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarlosChininin\FileUpload\Twig;
+
+use CarlosChininin\FileUpload\Model\FileUpload;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class FileUploadTwigExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly string $fileUploadDirectory
+    ) {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('file_upload_webpath', [$this, 'webpath']),
+        ];
+    }
+
+    public function webpath(?FileUpload $file): ?string
+    {
+        if (null === $file) {
+            return null;
+        }
+
+        return $this->fileUploadDirectory.$file->filePath();
+    }
+}
+{
+
+}
